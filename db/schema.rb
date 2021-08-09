1777c5e42950893ef8d453f7b1533fe506c276ca
Jenny <jennybell42@gmail.com>
2021-08-09 14:00:59 +0100

    users can sign up and log in

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2021_08_09_114423) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
